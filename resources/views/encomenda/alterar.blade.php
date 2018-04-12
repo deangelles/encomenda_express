6ef055b9403374d52edb38c397fdb5bf29f506d6
Deangelles Debastiani Cordeiro <deangelles123456@gmail.com>
2018-04-11 21:36:51 -0400

    Estrutura inicial 11/04/2016

--- resources/views/encomenda/alterar.blade.php
+++ resources/views/encomenda/alterar.blade.php
@@ -6,11 +6,51 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Alterar Encomenda</title>
+    <style>
+        /*
+        - Como Fazer Uma Imagem de Fundo Preencher Toda a Tela
+        - Autor: Guilherme Müller
+        - Site: http://guilhermemuller.com.br/blog/2011/06/08/como-fazer-uma-imagem-de-fundo-preencher-a-tela-inteira/
+        */
+
+        /* reset de margens */
+        * {
+            margin: 0;
+            padding:0;
+        }
+
+        /* para garantir que estes elementos ocuparão toda a tela */
+        body, html {
+            width: 100%;
+            height: 100%;
+            font-family: Arial, Tahoma, sans-serif;
+        }
+
+        body {
+            background: url(http://www.domidesign.com.br/wp-content/uploads/2015/12/verde-claro.jpg);
+
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+
+        #site {
+            width: 560px;
+            padding: 20px;
+            margin: 40px auto;
+            background: #FFF; /* fundo branco para navegadores que não suportam rgba */
+            background: rgba(255,255,255,0.8); /* fundo branco com um pouco de transparência */
+        }
+
+        p {
+            margin-bottom: 1.5em;
+        }</style>
     <link href="../../css/app.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
-    <h1 class="mt-2">Alterar encomenda</h1>
+    <h1 class="mt-2">Alterar Encomenda</h1>
     <form action="/encomenda/alterar" method="post" class="mt-2">
         <input type="hidden" name="_token" value="{{{ csrf_token() }}}">
         <div class="form-group">
